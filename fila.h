27c7ba9475efa1b8adc8393f0d918b1a78148d3a
bea <beatrizvianacosta16@gmail.com>
2022-09-13 09:35:10 -0300

    Update fila.h
    Implementação de fila apresentada na aula do dia 13/09, para o problema_gato_petisco.
    Para outras aplicações só mudar um pouquinho a estrutura do typedef.

--- fila.h
+++ fila.h
@@ -11,3 +11,11 @@ typedef struct {
 } pos;
 
 typedef pos item;
+
+fila *criafila();
+void destroifila(fila *f);
+int filaVazia(fila *f);
+item primeiroFila(fila *f);
+void insereFila(fila *f, item x);
+void aumentaFila(fila *f);
+item removeFil(fila *f);
