b04eb12e4066b26386c13b73d1e185fca4a5e779
bea <beatrizvianacosta16@gmail.com>
2022-09-15 10:22:40 -0300

    Update problema_gato_petisco.c
    Main feito na aula do dia 15/09.

--- problema_gato_petisco.c
+++ problema_gato_petisco.c
@@ -12,3 +12,55 @@
 //      *não tem solução.
 //   *se fila não vazia:
 //      *atual recebe primeira da fila. -> remove
+
+
+void labirinto (int **lab, int m, int n, pos gato, pos petisco) {
+    fila *caminho = criaFila();
+    int plinha[4] = [1, 0, -1, 0]
+    int pcoluna[4] = [0, 1, 0, -1]
+    int d;
+    pos atual, prox;
+
+    insereFila(caminho,  gato); //nem precisava colocar na fila, motivos didáticos,  primeiro passo: colocar o gato 
+    lab[gato.l][gato.c] = 1;
+    atual = removeFila(caminho);
+
+    while (diferente(atual, petisco)) {
+        for (d = 0; d < 4; d++) {
+            prox.l = atual.l + plinha[d];
+            prox.c = atual.c + pcoluna[d];
+
+            if (prox.l >= 0 && prox.l < m && prox.c >= 0 && prox.c < n && lab[prox.l][prox.c]) {
+                lab[prox.l][prox.c] = lab[atual.l][atual.c] + 1;
+                insereFila(caminho, prox);
+            }
+        }
+        
+        if (filaVazia(caminho)) {
+            printf("Não tem solução \n");
+            destroiFila(caminho);
+            return;
+        }
+        
+        atual = removeFila(caminho);
+    }
+    
+    imprimeMatriz(lab, m, n);
+    destroiFila(caminho);
+}
+
+diferente(pos a, pos b) {
+    if ([a.l] == [b.c] && [a.c] == [b.c]) return 1;
+    return 0;
+}
+
+imprimeMatriz (int **M, m, n) {
+    int i, j;
+
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < n; j++) {
+            printf("%d", M[i][j]);
+        }
+        printf("\n");
+    }
+}
